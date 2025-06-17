892597de38a55dd090aa1dc8ebb5a52a03e6cc41
RIM670 <rim.jouili@insat.ucar.tn>
2025-06-17 21:20:54 +0200

    commit 2

--- bookstore/src/main/resources/data.sql
+++ bookstore/src/main/resources/data.sql
@@ -1,7 +1,6 @@
-
+DELETE FROM book;
 
 INSERT INTO book (id, title, author, price) VALUES
 (1, 'Le Petit Prince', 'Antoine de Saint-Exupéry', 10.99),
 (2, '1984', 'George Orwell', 9.50),
 (3, 'Clean Code', 'Robert C. Martin', 35.00);
-
